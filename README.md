05ec732d1467788cb273ddf10be8cec819c5c5f8
Debora <deborarebelatto1@gmail.com>
2021-08-17 10:33:09 -0300

    Added batalha.asm

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Trabalho Organização de Computadores 2021
 ## Jogo de Batalha Naval
+
+- [Relatório](https://docs.google.com/document/d/1uxDTAzi0NaxogOyYGXjQHqikmh5c8k7BtDaZQ4Lr6yA/edit#)
+
---
+++ batalha.asm
@@ -0,0 +1,15 @@
+# # # # # # # # # # # # # # # # # # # # # # # #
+# Universidade Federal da Fronteira Sul
+# Ciência da Computação
+# Organização de Computadores
+# Professor: Luciano Caimi
+# Débora Rebelatto - 172101034
+# Victor
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+  .data
+
+  .global main
+
+  .text
+main:
