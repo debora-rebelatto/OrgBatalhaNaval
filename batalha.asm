fb74ac47123b87aaa6c36ccd30e701ad79685cb8
Debora R <deborarebelatto1@gmail.com>
2021-10-03 23:07:21 -0300

    3/10

--- batalha.asm
+++ batalha.asm
@@ -29,15 +29,14 @@
 #   posição do último tiro disparado e;
 # ❌ 3) os recordes do jogo que consiste no menor número de tiros para afundar todas as embarcações de algum jogo anterior.
 
-
 ####################################################
 # AO = valores temporários para leitura
 # A1 = Máx embarcações
 # A2 = Contador de Tiros
 # A3 = Contador acertos
 # A4 = Contador afundados
-# A5 =
-# A6 =
+# A5 = Recorde Tiros
+# A6 = Recorde Acertos
 # A7 = valores temporários para leitura
 
 # T0 = Inicio Contador / Menu 1
@@ -48,33 +47,34 @@
 # T5 = Registrador para comparação
 # T6 =
 
-# S0 = Início contador
-# S1 = Matriz Batalha
+# S0 = Matriz Batalha
+# S1 = 
 # S2 = 
 # S3 =
 # S4 = 
 # S5 =
-# S6 = 
-# S7 = 
+# S6 =
+# S7 =
 # S8 =
-# S9 =
+# S9 = Recorde Afundados
 # S10 = Linha
 # S11 =	Coluna
 ####################################################
 
 	.data
-matriz_batalha:				.space	324
-matriz_interface: 		.word	-1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1
+matriz_batalha:					.space	324
+matriz_tiros: 					.word	-1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1,  -1,-1,-1,-1,-1,-1,-1,-1
 
 ################## Mensagens do jogo ##################
 start_game:							.asciz  "\n-------- Batalha Naval --------\n"
+matriz_header:					.asciz  "\n----------- Matriz -----------\n"
 msg_espaco:							.asciz  " "
 msg_enter:							.asciz  "\n"
 msg_pos_invalida:       .asciz  "\nPosicao Invalida\n"
 msg_dimensao_invalida:  .asciz  "\nValores fora do limite!\n"
 msg_sobreposicao_navio: .asciz  "\nNavio colidido!\n"
-msg_cima:								.asciz  "\n- 0 1 2 3 4 5 6 7\n"
-msg_lateral:						.word	0, 1, 2, 3, 4, 5, 6, 7
+msg_cima:								.asciz  "\n- 0 1 2 3 4 5 6 7 8 9\n"
+msg_lateral:						.word	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 msg_menu:								.asciz "\n------------------Menu------------------\n    1 - Reiniciar Jogo\n    2 - Mostrar estado atual da Matriz\n    3 - Nova jogada\n    4 - Sair do jogo\n"
 
@@ -99,16 +99,11 @@ msg_recordTiros:				.asciz "  Recorde de tiros: "
 msg_recordeAcertos: 		.asciz "  Recorde de acertos: "
 msg_recordeAfundados: 	.asciz "  Recorde de navios afundados: "
 
-msg_exit: 							.asciz "Finalizado\n"
+msg_exit: 							.asciz "\nFinalizado\n"
 msg_tiro_linha: 				.asciz "\nLinha do tiro: \n"
 msg_tiro_coluna: 				.asciz "\nColuna do tiro: \n"
 
-salva_S0:								.word	0
-salva_ra:								.word	0
-salva_ra1:							.word	0
-
 msg_traco:							.asciz  "-"
-msg_bandeira:						.asciz  "F"
 
 msg_ganhou:							.asciz  "\n\nParabéns!\nVocê ganhou.\n"
 horizontal:							.asciz 	"\nhorizontal\n"
@@ -122,21 +117,31 @@ main:
 	li a7, 4 								# Imprime msg
 	ecall
 
+#######################################################
+# função: get_info_embarcacoes
+# entrada:
+# saída:
+#######################################################
 get_info_embarcacoes:
 	la a0, msg_num_navio 		# Carrega msg 
 	li a7, 4 								# Imprime msg
 	ecall
-	
+
 	addi a7, zero, 5 				# Le valor inserido pelo usuario
 	ecall
-	add t0, zero, a0 				# Carrega número de embarcações em t0 - Final Contador
+	add a1, zero, a0 				# A1 = Número de embarcações/Final Contador
 
   la a0, msg_enter
   li a7, 4
   ecall
-  
+
+#######################################################
+# função: loop
+# entrada:
+# saída:
+#######################################################
 loop:
-	beq s0, t0, inicializa
+	beq t0, a1, inicializa
 
 perguntas:
 	pergunta_disposicao:
@@ -176,11 +181,21 @@ perguntas:
 		add t4, zero, a0 				# T4 = Linha
 		bgt t4, t5, erro_coluna	# erro
 
+#######################################################
+# função: insere_embarcacoes
+# entrada:
+# saída:
+#######################################################
 insere_embarcacoes:
 	addi t5, zero, 1					# Valor para comparação
 	beq t1, t5, navio_vertical
 	beq t1, zero, navio_horizontal
 
+#######################################################
+# função: navio_horizontal
+# entrada:
+# saída:
+#######################################################
 navio_horizontal:
 	la a0, horizontal
 	li, a7, 4
@@ -194,9 +209,15 @@ navio_vertical:
 	ecall
 
 atualiza:
-	addi s0, s0, 1
+	addi t0, t0, 1
 	j loop
-	
+
+#######################################################
+# Erros:
+#		erro_linha
+#		erro_coluna
+#		erro_disposicao
+#######################################################
 erro_linha:
 	la a0, msg_erro_posicao
 	li, a7, 4
@@ -215,25 +236,37 @@ erro_disposicao:
 	ecall
 	j pergunta_disposicao
 
+#######################################################
+# função: inicializa
+# entrada:
+#		a2 ⇒ Contador de Tiros
+#		a3 ⇒ Contador de Acertos
+#		a4 ⇒ Contador de Afundados
+#		s0 ⇒ Matriz de Batalha
+#		s1 ⇒ Número de Coluna/Linhas
+#		s2 ⇒ Tamanho da matriz
+# saída:
+#######################################################
 inicializa:
-	addi a2, zero, 0					# Contador de tiros
-	addi a3, zero, 0					# Contador acertos
-	addi a4, zero, 0					# Contador afundados
+	addi 	a2, zero, 0					# A2 = Contador de tiros
+	addi 	a3, zero, 0					# A3 = Contador acertos
+	addi 	a4, zero, 0					# A4 = Contador afundados
+	la		s0, matriz_batalha	# S0 = Matriz
+	li 		s1, 8								# S1 = Número de colunas/linhas
+	mul		a2, s1, s1					# S2 ⇒ Tamanho da matriz
 
-	# la	a6, matriz_batalha	# Carrega o endereço da matriz campo em a6
-	# li 	a2, 8								# Carrega o número de colunas/linhas
-	# mul	a1, a2, a2					# Calcula o tamanho da matriz
-	
-	# li	t0, 0								# Inicializa o contador que irá percorrer a matriz
-	# li	t1, 9								# Inicializa o comparador de bomba
-	# li	t2, 0			# Inicializa o contador secundário que irá contar as colunas da matriz
-	# li	t3, 0			# Inicializa o comparador da coluna 0
-	# li	t4, 7			# Inicializa o comparador da coluna 7
-	# li	t5, 0			# Inicializa o contador terciário que irá contar as linhas da matriz
-	# li	a4, 0			# Inicializa o contador de posições abertas
-	# li	a5, 0			# Inicializa o contador de bombas da matriz
 	j menu
 
+#######################################################
+# função: menu
+#		Lê entrada de usuário para fazer transições
+# entrada:
+#		t0 ⇒ 1
+#		t1 ⇒ 2
+#		t2 ⇒ 3
+#		t3 ⇒ 4
+# saída:
+#######################################################
 menu:
 	addi t0, zero, 1	# T0 = 1
 	addi t1, zero, 2	# T1 = 2
@@ -253,16 +286,85 @@ menu:
 	beq t2, a0, nova_jogada			# branch if equal to nova_jogada
 	beq t3, a0, exit						# branch if equal to exit
 
+#######################################################
+# função: reiniciar_jogo
+# entrada:
+# saída:
+#######################################################
 reiniciar_jogo:
 	# Reiniciar matrizes
 	jal get_info_embarcacoes
-	
-estado_matriz:
-	# Imprime Matriz
+
+imprime_matriz:
 	la	a0, msg_cima			# Carrega a mensagem das marcações que vão na parte de cima da matriz
 	li	a7, 4							# Imprime a mensagem
+	ecall 
+	li	t0, 0					# Inicializa o contador que irá percorrer a matriz
+	li	t1, 9					# Inicializa o comparador de coluna para impressão da matriz
+	mul	a6, t1, t1		# Calcula o tamanho da matriz
+	la	s1, matriz_batalha	# Carrega o endereço da matriz campo em s1
+	la	t3, msg_lateral		# Carrega o endereço da mensagem que ira ser impressa na lateral da matriz para indicar a marcação das posições
+	j	imprimefirst
+
+# Função que imprime a linha marcadora das posições da matriz na parte de cima, e inicia com o 0 na lateral
+imprimefirst:
+	lw a0, 0(t3)		# Carrega o primeiro valor da mensagem que vai na lateral da matriz para marcar as posições
+	li a7, 1			# Imprime o valor
 	ecall
 
+	addi	t3, t3, 4		# Acessa o próximo valor da mensagem
+	j 	imprime
+
+imprime:
+		beq t0, a6, standings			# Realiza a repetição da função conta em todos os indices da matriz
+		beq	t0, t1, pula			# Se o contador for igual a 7, pula para a função pula
+
+		addi s0, s0, 4			# Acrescenta 4 no endereço da matriz, acessa o próximo número
+		addi t0, t0, 1			# Acrescenta 1 no contador
+		j	imprime
+		
+		pula:
+			la	a0, msg_enter		# Carrega a mensagem enter, para pular a linha
+			li	a7, 4						# Imprime a mensagem
+			ecall
+			addi	t1, t1, 8			# Acrescenta o número da próxima quebra de linha na matriz
+
+			lw  a0, 0(t3)			# Carrega o primeiro valor da mensagem que vai na lateral da matriz para marcar as posições
+			li 	a7, 1						# Imprime o valor
+			ecall
+
+			addi	t3, t3, 4			# Acessa o próximo valor da mensagem
+			j 	imprime
+
+#######################################################
+#	função: estado_matriz
+# entrada:
+# saída:
+#######################################################
+estado_matriz:
+	la a0, matriz_header
+	li a7, 4
+	ecall
+
+	j imprime_matriz
+
+#######################################################
+# função: standings
+# entrada:
+#		a2	⇒ Tiros
+#		a3	⇒ Acertos
+#		a4	⇒ Afundados
+#		a5	⇒ Recorde Tiros
+#		a6	⇒ Recorde Acertos
+#		s9 	⇒ Recorde Afundados
+#		s10 ⇒ Último tiro linha
+#		s11 ⇒ Último tiro coluna
+#######################################################
+standings:
+	la a0, msg_enter
+	li a7 4
+	ecall
+	
 	# Current Game
 	la a0, msg_currentVoce
 	li a7, 4
@@ -321,18 +423,65 @@ estado_matriz:
 	addi a0, s11, 0
 	li a7, 1
 	ecall
-	
+
 	la a0, msg_enter
 	li a7 4
 	ecall
 
-	j menu
+	# Recordes
+	la a0, msg_recordes
+	li a7 4
+	ecall
+
+		# Recorde Tiros
+	la a0, msg_recordTiros
+	li a7 4
+	ecall
+	addi a0, a5, 0
+	li a7, 1
+	ecall
+	la a0, msg_enter
+	li a7 4
+	ecall
 	
+		# Recorde Acertos
+	la a0, msg_recordeAcertos
+	li a7 4
+	ecall
+	addi a0, a6, 0
+	li a7, 1
+	ecall
+	la a0, msg_enter
+	li a7 4
+	ecall
+	
+		# Recorde Afundados
+	la a0, msg_recordeAfundados
+	li a7 4
+	ecall
+	addi a0, s9, 0
+	li a7, 1
+	ecall
+	la a0, msg_enter
+	li a7 4
+	ecall
+	
+	j menu
+
+#######################################################
+# função: nova_jogada
+# 	Faz tiros e incrementa contador de tiros
+# entrada:
+#		a2	⇒ Tiros
+#		s10 ⇒ Linha
+#		s11 ⇒ Coluna
+# saída:
+#######################################################
 nova_jogada:
 	la a0, msg_tiro_linha
 	li a7, 4
 	ecall
-	
+
 	addi	a7, zero, 5 		# Lê o valor da linha inserido pelo jogador
 	ecall
 
@@ -346,11 +495,16 @@ nova_jogada:
 	ecall
 
 	add s11, zero, a0 		# S11 =  Coluna
-	addi a1, a1, 1
+	addi a2, a2, 1				# Atualiza contador de tiros
 	
 	j menu
 
+#######################################################
+# função: exit
+#		Finaliza jogo
+#######################################################
 exit:
 	la a0, msg_exit
 	li a7, 4
 	ecall
+	nop
